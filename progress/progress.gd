748dab356df6a5794010ac24539a93cfb150e22c
Greizgh <greizgh@ephax.org>
2016-05-09 08:31:13 +0200

    0.0.1 preview

--- progress/progress.gd
+++ progress/progress.gd
@@ -1,14 +1,12 @@
 extends Node2D
 
-var progress
-var sprite
+onready var progress = get_node("progress")
+onready var sprite = get_node("sprite")
 var ini_pos
-var g
+onready var g = get_node("/root/global")
+onready var label = get_node("timer_label")
 
 func _ready():
-	progress = get_node("progress")
-	sprite = get_node("sprite")
-	g = get_node("/root/global")
 	ini_pos = sprite.get_pos()
 	set_process(true)
 
@@ -19,3 +17,5 @@ func _process(delta):
 	var val = g.play_time/progress.get_max()
 	progress.set_val(g.play_time)
 	sprite.set_pos(ini_pos + Vector2(val * progress.get_size().x, 0))
+	var remaining = progress.get_max() - g.play_time
+	label.set_text(str(floor(remaining)))
