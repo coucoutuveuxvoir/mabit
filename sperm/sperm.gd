401c6035749f01fcf8508713e95fa2ef31bb4850
Greizgh <greizgh@ephax.org>
2016-07-07 08:06:52 +0200

    Increase load velocity

--- sperm/sperm.gd
+++ sperm/sperm.gd
@@ -1,10 +1,12 @@
 extends KinematicBody2D
 
-const velocity = 200
+const base_velocity = 300
+var velocity = 0
 
 func _ready():
 	set_meta('jizz', true)
 	set_process(true)
+	velocity = base_velocity + rand_range(-50, 50)
 	
 func _process(delta):
 	move(Vector2(0, -delta*velocity))
