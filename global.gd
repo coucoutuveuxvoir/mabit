98206ed082cf17541dbf364ec86f6fc15c5f8637
Greizgh <greizgh@ephax.org>
2016-07-12 08:08:43 +0200

    Save and restore highscore

--- global.gd
+++ global.gd
@@ -6,6 +6,8 @@ var current_scene = null
 var score = 0
 var play_time = 0
 var first_time = false
+var highscore_names = ["Zob", "Zob", "Zob", "Zob", "Zob"]
+var highscore_scores = [500, 300, 250, 100, 10]
 
 func _ready():
 	var root = get_tree().get_root()
@@ -26,7 +28,8 @@ func save_game():
 	var savegame = File.new()
 	savegame.open_encrypted_with_pass(SAVEFILE, File.WRITE, OS.get_unique_ID())
 	var data = {
-		score=score
+		highscore_names=highscore_names,
+		highscore_scores=highscore_scores
 	}
 	savegame.store_var(data)
 	savegame.close()
@@ -39,5 +42,6 @@ func load_game():
 	var curLine = {}
 	savegame.open_encrypted_with_pass(SAVEFILE, File.READ, OS.get_unique_ID())
 	var data = savegame.get_var()
-	print(data['score'])
+	highscore_names = data["highscore_names"]
+	highscore_scores = data["highscore_scores"]
 	savegame.close()
--- menu/menu.gd
+++ menu/menu.gd
@@ -8,9 +8,9 @@ func _on_play_btn_pressed():
 	get_node("/root/global").goto_scene("res://main.scn")
 
 
-func _on_settings_btn_pressed():
-	pass
-
-
 func _on_tutorial_btn_pressed():
 	get_node("/root/global").goto_scene("res://tutorial/tutorial.scn")
+
+
+func _on_score_btn_pressed():
+	get_node("/root/global").goto_scene("res://highscores/highscores.scn")
